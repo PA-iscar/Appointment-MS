7dead8a0d4779d8f94307a387c16b7b00d87e59d
Abhishek <abhiaro27@gmail.com>
2022-04-14 08:50:41 +0530

    changes to deploy

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => res.status(200).json("Connection Success"));
-
+app.get("/", (req, res) => res.status(200).json("Loading Success"));
+app.get("/check", (req, res)=> res.status(200).json("Connection success"))
 app.use("/lot", lotController);
 app.use("/vehicle", vehicleController);
 // app.use("/history", historyController);
