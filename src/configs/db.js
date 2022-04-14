98a1435e15f6b60495071a9ca91c71f270cfaa64
Abhishek <abhiaro27@gmail.com>
2022-04-14 08:46:37 +0530

    changes to deploy

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const DB_URL = process.env.DB_URL;
+const DB_URL =
+  process.env.DB_URL ||
+  "mongodb+srv://abhishek:something@parkinglot.zmq7a.mongodb.net/parkinglotDB?retryWrites=true&w=majority";
 
 module.exports = () => {
   return mongoose.connect(DB_URL);
